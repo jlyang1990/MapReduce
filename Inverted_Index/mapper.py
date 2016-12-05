d1f4c5586ec2643650819dc5769f418d748b1747
jlyang1990 <jlyang@ucdavis.edu>
2016-12-04 23:01:04 -0800

    update code file

--- Inverted_Index/mapper.py
+++ Inverted_Index/mapper.py
@@ -7,7 +7,7 @@ import re
 reader = csv.reader(sys.stdin, delimiter = '\t')
 
 for line in reader:
-    if len(line) == 19:
+    if len(line) == 19 and line[0] != "id":
         words = re.compile('[a-zA-Z]+').findall(re.sub('<[^>]*>', '', line[4].lower()))
         for word in words:
 	    print "{0}\t{1}".format(word, line[0])
